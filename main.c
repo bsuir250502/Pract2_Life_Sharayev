4eb94a5a4b5d8ad886563ac0cc9dfbed77443fbf
sj2903 <sj2903@mail.ru>
2013-04-09 14:58:06 +0300

    Added reading information from file

--- main.c
+++ main.c
@@ -1,8 +1,19 @@
 #include <stdafx.h>
 #include <stdlib.h>
 
-int main() {
-  int i,j;
 
+
+int main() {
+  int i, j, size_x, size_y, *map;
+  char input_buffer[128], *endptr;
+  FILE *fp = fopen("input.in", "r");
+  fgets(input_buffer, strlen(input_buffer), fp);
+  size_x = strtol(input_buffer, &endptr, 10);
+  fgets(input_buffer, strlen(input_buffer), fp);
+  size_y = strtol(input_buffer, &endptr, 10);
+  map = (int *)malloc(size_x * size_y * 2 * sizeof(int));
+
+
+  fclose(fp);
   return 0;
 }
