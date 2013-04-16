8a18c4b234f85f04c9becd7a2aa1e6b2f7d0e027
sj2903 <sj2903@mail.ru>
2013-04-17 02:34:19 +0300

    Fixed code

--- main.c
+++ main.c
@@ -9,35 +9,39 @@ typedef struct {
 
 
 
-map_t *filling_map(int *, int *);
+map_t **filling_map(int *, int *);
 
 int main()
 {
     int size_x = 0, size_y = 0;
-    map_t *map;
+    map_t **map;
     map = filling_map(&size_x, &size_y);
-
-
-
 
     return 0;
 }
 
-map_t *filling_map(int *size_x, int *size_y)
+map_t **filling_map(int *size_x, int *size_y)
 {
     int i, j;
     char input_buffer[128], *endptr;
-    map_t *map;
+    map_t **map;
     FILE *fp = fopen("input.in", "r");
 
     fgets(input_buffer, strlen(input_buffer), fp);
     *size_x = strtol(input_buffer, &endptr, 10);
     fgets(input_buffer, strlen(input_buffer), fp);
     *size_y = strtol(input_buffer, &endptr, 10);
-    map = (masp_t *) malloc((*size_x) * (*size_y) * sizeof(map));
+    map = (map_t **) malloc((*size_x) * (*size_y) * sizeof(**map));
     if (!map) {
         printf("Memory isn't allocated");
         exit(1);
+    }
+    for(i = 0; i < *size_y; i++) {
+        map[i] = (map_t *) malloc(sizeof(*map));
+        if (!map[i]) {
+            printf("Memory isn't allocated");
+            exit(1);
+        }
     }
     for (i = 0; i < *size_y; i++) {
         for (j = 0; j < *size_x; j++) {
@@ -45,8 +49,6 @@ map_t *filling_map(int *size_x, int *size_y)
         }
         getc(fp);
     }
-
-
 
     fclose(fp);
     return map;
