f3b63133f7242cf37aca9eaa6fdd7fec3afe985a
Eugen <sj2903@mail.ru>
2013-04-17 14:52:45 +0300

    fixed reading from file, added define and reading of generation number

--- main.c
+++ main.c
@@ -2,47 +2,56 @@
 #include <stdlib.h>
 #include <string.h>
 
+#define SIZE( x ) (sizeof(x)/sizeof(*x))
+
 typedef struct {
     int present;
     int next;
 } map_t;
 
-map_t **filling_map(int *, int *);
+map_t **filling_map(int *, int *, int *);
 int free_memory(map_t **, int);
 int display_map(map_t **, int, int);
 
 int main()
 {
-    int sizeX = 0, sizeY = 0;
+    int i, sizeX = 0, sizeY = 0, gen_num = 0;
     map_t **map;
-    map = filling_map(&sizeX, &sizeY);
+    map = filling_map(&sizeX, &sizeY, &gen_num);
     display_map(map, sizeX, sizeY);
+    for(i = 0; i < gen_num; i++) {
+        printf("something")
+    }
+
     free_memory(map, sizeY);
     if (!map) {
-        printf("Memory free");
+        printf("Memory free\n");
         exit(1);
     }
     return 0;
 }
 
-map_t **filling_map(int *sizeX, int *sizeY)
+map_t **filling_map(int *sizeX, int *sizeY, int *gen_num)
 {
     int i, j;
-    char input_buffer[128], *endptr;
+    char input_buffer[128];
     map_t **map;
     FILE *fp = fopen("input.in", "r");
 
-    fgets(input_buffer, strlen(input_buffer), fp);
-    *sizeX = strtol(input_buffer, &endptr, 10);
-    fgets(input_buffer, strlen(input_buffer), fp);
-    *sizeY = strtol(input_buffer, &endptr, 10);
+    fgets(input_buffer, SIZE(input_buffer), fp);
+    *sizeX = strtol(input_buffer, NULL, 10);
+    fgets(input_buffer, SIZE(input_buffer), fp);
+    *sizeY = strtol(input_buffer, NULL, 10);
+    fgets(input_buffer, SIZE(input_buffer), fp);
+    *gen_num = *sizeY = strtol(input_buffer, NULL, 10);
+
     if (!(map = (map_t **) calloc(*sizeY, sizeof(**map)) )) {
-        printf("Memory isn't allocated");
+        printf("Memory isn't allocated at the first calloc\n");
         exit(1);
     }
     for(i = 0; i < *sizeY; i++) { 
         if (!(map[i] = (map_t *) calloc(*sizeX, sizeof(*map)) )) {
-            printf("Memory isn't allocated");
+            printf("Memory isn't allocated\n");
             exit(1);
         }
     }
@@ -64,7 +73,6 @@ int free_memory(map_t **map, int sizeY)
         free(map[i]);
     }
     free(map);
-
     return 0;
 }
 
