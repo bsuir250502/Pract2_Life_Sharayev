20787c3b5833e67eaa143ab876565439a8829a75
sj2903 <sj2903@mail.ru>
2013-04-17 14:34:49 +0300

    Added print and display map functions

--- main.c
+++ main.c
@@ -7,20 +7,25 @@ typedef struct {
     int next;
 } map_t;
 
-
-
 map_t **filling_map(int *, int *);
+int free_memory(map_t **, int);
+int display_map(map_t **, int, int);
 
 int main()
 {
-    int size_x = 0, size_y = 0;
+    int sizeX = 0, sizeY = 0;
     map_t **map;
-    map = filling_map(&size_x, &size_y);
-
+    map = filling_map(&sizeX, &sizeY);
+    display_map(map, sizeX, sizeY);
+    free_memory(map, sizeY);
+    if (!map) {
+        printf("Memory free");
+        exit(1);
+    }
     return 0;
 }
 
-map_t **filling_map(int *size_x, int *size_y)
+map_t **filling_map(int *sizeX, int *sizeY)
 {
     int i, j;
     char input_buffer[128], *endptr;
@@ -28,23 +33,21 @@ map_t **filling_map(int *size_x, int *size_y)
     FILE *fp = fopen("input.in", "r");
 
     fgets(input_buffer, strlen(input_buffer), fp);
-    *size_x = strtol(input_buffer, &endptr, 10);
+    *sizeX = strtol(input_buffer, &endptr, 10);
     fgets(input_buffer, strlen(input_buffer), fp);
-    *size_y = strtol(input_buffer, &endptr, 10);
-    map = (map_t **) malloc((*size_x) * (*size_y) * sizeof(**map));
-    if (!map) {
+    *sizeY = strtol(input_buffer, &endptr, 10);
+    if (!(map = (map_t **) calloc(*sizeY, sizeof(**map)) )) {
         printf("Memory isn't allocated");
         exit(1);
     }
-    for(i = 0; i < *size_y; i++) {
-        map[i] = (map_t *) malloc(sizeof(*map));
-        if (!map[i]) {
+    for(i = 0; i < *sizeY; i++) { 
+        if (!(map[i] = (map_t *) calloc(*sizeX, sizeof(*map)) )) {
             printf("Memory isn't allocated");
             exit(1);
         }
     }
-    for (i = 0; i < *size_y; i++) {
-        for (j = 0; j < *size_x; j++) {
+    for (i = 0; i < *sizeY; i++) {
+        for (j = 0; j < *sizeX; j++) {
             map[i][j].present = getc(fp) - '0';
         }
         getc(fp);
@@ -52,4 +55,28 @@ map_t **filling_map(int *size_x, int *size_y)
 
     fclose(fp);
     return map;
+}
+
+int free_memory(map_t **map, int sizeY)
+{
+    int i;
+    for(i = 0; i < sizeY; i++) {
+        free(map[i]);
+    }
+    free(map);
+
+    return 0;
+}
+
+int display_map(map_t **map, int sizeX, int sizeY)
+{
+    int i, j;
+    for (i = 0; i < sizeY; i++) {
+        for (j = 0; j < sizeX; j++) {
+            printf("%d", map[i][j].present);
+        }
+        puts("");
+    }
+
+    return 0;
 }
